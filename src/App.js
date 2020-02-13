9f474007b48bba85e5bd1c1194228d643d289b9c
Rama <rama.p@sekon.com>
2020-02-13 14:26:59 -0500

    1.12*: anecdotes step1

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
 import React, { useState } from 'react'
-import Header from './component/Header.js'
 import Button from './component/Button.js'
-import Stats from './component/Stats.js'
 
-const App = () => {
-  const [ feedback, setFeedback ] = useState({
-    good: 0, bad: 0, neutral: 0
-  })
-  let newFeedback = {
-    good: feedback.good,
-    bad: feedback.bad,
-    neutral: feedback.neutral
-  } 
-  const setValue = (value) => {      
-    switch (value) {
-      case 'good':
-        newFeedback = {
-          good: feedback.good + 1,
-          bad: feedback.bad,
-          neutral: feedback.neutral
-        }
-        break;          
-      case 'bad':
-        newFeedback = {
-          good: feedback.good,
-          bad: feedback.bad + 1,
-          neutral: feedback.neutral
-        }
-        break;
-      case 'neutral':
-        newFeedback = {
-          good: feedback.good,
-          bad: feedback.bad,
-          neutral: feedback.neutral + 1
-        }
-        break;
-      default:
-        break;
-    }
-    //console.log(newFeedback)
-    return (
-      setFeedback(newFeedback)  
-    )
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const getQuote = () => {
+    setSelected(Math.floor((Math.random()*props.anecdotes.length)))
   }
-
   return (
     <div>
-      <Header course="give feedback" />
-      <Button handler={()=>setValue('good')} text=" good " />
-      <Button handler={()=>setValue('neutral')} text=" neutral " />
-      <Button handler={()=>setValue('bad')} text=" bad " />
-      <Header course="statistics" />
-      <Stats stats={feedback} />
+      <p>{props.anecdotes[selected]}</p>
+      <Button handler={()=>getQuote()} text="next anecdote" />
     </div>
   )
+  
 }
 
 
