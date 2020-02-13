b0e1439977c0ba6ba57638084c3c0b383e9411ca
Rama <rama.p@sekon.com>
2020-02-13 14:32:32 -0500

    1.13*: anecdotes step2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Button from './component/Button.js'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))
+
   const getQuote = () => {
     setSelected(Math.floor((Math.random()*props.anecdotes.length)))
   }
+
+  const addVote = (value) => {
+    let temp = [...votes]
+    temp[value]++
+    setVotes(temp)
+  }
+
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handler={()=>addVote(selected)} text="vote" />
       <Button handler={()=>getQuote()} text="next anecdote" />
     </div>
   )
