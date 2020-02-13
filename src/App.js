0f3556e4d9304c2c205593e3f05b8a8ad7456b9e
Rama <rama.p@sekon.com>
2020-02-13 08:18:15 -0500

    1.1: course information, step1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './component/Header.js'
+import Content from './component/Content.js'
+import Total from './component/Total.js'
 
-function App() {
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header course={course} />
+      <Content part1={part1} ex1={exercises1} part2={part2} ex2={exercises2} part3={part3} ex3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </>
+  )
 }
+
 
 export default App;
---
+++ src/component/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Content = (props) => {
+    return (
+        <>
+        <p>{props.part1} {props.ex1}</p>
+        <p>{props.part2} {props.ex2}</p>
+        <p>{props.part3} {props.ex3}</p>
+        </>
+    )
+}
+
+export default Content
